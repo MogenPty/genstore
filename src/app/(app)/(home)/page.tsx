2ab162a0772ea28fed8a6af0e7c88ed1088a126b
Real Solly M <12409199+RealSollyM@users.noreply.github.com>
2025-07-06 13:32:36 +0200

    feat: update package.json with new scripts and dependencies
    - Added new script for generating TypeScript types using bunx.
    - Introduced a database refresh script.
    - Updated dependencies for @payloadcms packages to version 3.45.0.
    - Added tsx as a dev dependency.
    
    feat: enhance Home page to load subcategories
    
    - Modified the payload.find query to include a depth parameter for loading subcategories.
    - Added a condition to only fetch categories without a parent.
    
    feat: extend Categories collection schema
    
    - Added new fields: slug (unique), color, parent (relationship), and subcategories (join).
    - Enhanced the structure of the Categories collection.
    
    feat: update payload types for categories and user selections
    
    - Introduced new interfaces for Category and its select options.
    - Updated Config interface to include new collections and their select options.
    - Enhanced User interface with session management.

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -8,6 +8,12 @@ export default async function Home() {
 
   const data = await payload.find({
     collection: "categories",
+    depth: 1, //load subcategories
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
   });
 
   return <div className="p-4 space-y-4">{JSON.stringify(data, null, 2)}</div>;
