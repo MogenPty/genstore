d5895ef2f5635e58cbb4bb260b34f5e131e24a1a
Real Solly M <12409199+RealSollyM@users.noreply.github.com>
2025-08-20 22:42:49 +0200

    refactor(auth): streamline sign-in and sign-up page logic

--- src/app/(app)/(library)/library/page.tsx
+++ src/app/(app)/(library)/library/page.tsx
@@ -1,7 +1,10 @@
-import { GENSTORE_PAGE_LIMIT } from "@/constants";
-import { LibraryView } from "@/modules/library/ui/view/library-view";
-import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { GENSTORE_PAGE_LIMIT } from "@/constants";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { LibraryView } from "@/modules/library/ui/view/library-view";
+
+export const dynamic = "force-dynamic";
 
 const LibraryPage = async () => {
   const queryClient = getQueryClient();
